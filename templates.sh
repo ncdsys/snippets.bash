f932ddb9b1297b1d1274553846404a44837e3750
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-02-02 02:33:26 -0200

    Adding snippets.

--- templates.sh
+++ templates.sh
@@ -994,5 +994,12 @@ modprobe <module> -o <new_name>
 # unload a module from the linux kernel.
 modprobe -r <module>
 ##############################################################################
+# list, process, running, executed, target, port.
+# list processes running on a given target-port whose protocol is tcp.
+# example:
+# it would list vy if it were connected to a given host through the port 6667.
+# lsof -i tcp:6667
 
+lsof -i <protocol>:<port>
+##############################################################################
 
