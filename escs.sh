4f2a3b420b0b3a6c9d05360ac0f65042e123af1a
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-04-30 14:40:53 -0300

    Adjusting escs.sh.

--- escs.sh
+++ escs.sh
@@ -1,12 +1,9 @@
-# clone, snip.bash, github, repository.
-cd ~/projects/
-git clone git@github.com:iogf/snip.bash.git snip.bash-code
-
 # push, snip, bash.
-cd ~/projects/snip.bash-code
+cd ~/projects/snippets.bash-code
 git status
 git add *
 git commit -a
 git push
+
 
 
