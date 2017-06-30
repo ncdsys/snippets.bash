d8215bd7008fe030fe5f355e7c2ac3154192960e
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-06-30 18:26:03 -0300

    Adding escs.

--- templates.sh
+++ templates.sh
@@ -375,10 +375,10 @@ ffmpeg -f alsa -i default -f x11grab -s 1366x768 -r 30 -i :0.0 -qscale 0 <filena
 # low quality.
 ffmpeg  -f x11grab -s 1366x768 -r 30 -i :0.0 -qscale 0 filename.avi
 ##############################################################################
-# capture, record, video, desktop, ffmpeg, without, audio, high, quality.
+# capture, record video from desktop using ffmpeg without audio and high quality.
 ffmpeg -f x11grab -r 25 -s 1280x720 -i :0.0+0,24 -vcodec libx264 -threads 0 <video.mkv>
 ##############################################################################
-# capture, record, video, desktop, ffmpeg, without, audio, high, audio, quality, with.
+# capture, record video from desktop using ffmpeg with audio and high quality.
 ffmpeg -f alsa -i default -f x11grab -r 25 -s 1280x720 -i :0.0+0,24 -acodec pcm_s16le -vcodec libx264 -threads 0 <output.mkv>
 
 ##############################################################################
@@ -1126,4 +1126,5 @@ exit
 ##############################################################################
 # record wav/sounds on linux, wav.
 arecord -vv -f cd test.wav
+
 
